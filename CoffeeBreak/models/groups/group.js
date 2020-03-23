1bea9f7b80b75d557738d65401595d89c150b372
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-03-23 11:11:27 -0400

    add group model

---
+++ CoffeeBreak/models/groups/group.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const postSchema = new Schema({
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    content : {
+        type : String
+    },
+    picture : {
+        type : String
+    }
+},{
+    timestamps : true
+})
+const groupSchema = new Schema({
+    name : {
+        type : name,
+        required : true
+    },
+    user : {
+        type : Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    post : [postSchema]
+},{
+    timestamps : true,
+})
+
+
+module.exports = mongoose.model('Group', groupSchema);
